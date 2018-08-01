a295f9af1578ae95e3f1b9ad7e611d56aa835f16
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2018-08-01 20:39:15 +0100

    [PCF] Refactored Repl to use Map

--- PCF/Repl.hs
+++ PCF/Repl.hs
@@ -5,20 +5,21 @@ import Parser
 
 import System.IO           (hFlush, stdout)
 import Control.Monad       (guard)
+import qualified Data.Map.Lazy as M 
 
 -- top level repl function
 replMain :: IO ()
 replMain = do
   putStrLn "Welcome to the PCF REPL"
   putStrLn "Type some terms or press Enter to leave."
-  repl []
+  repl M.empty
 
 -- stores variables from let expressions at runtime
-type Environment = [(String,PCFTerm)]
+type Environment = M.Map String PCFTerm
 
 -- REPL loop, takes input reduces and prints result, or exits out
 repl :: Environment -> IO ()
-repl ctx = do
+repl env = do
   putStr ">   "
   hFlush stdout
   s <- getLine
@@ -33,7 +34,7 @@ repl ctx = do
           Just y -> mapM_ putStrLn . prependReductions x' $ reductions x'
           _ -> cannotType $ tail s
         else cannotParse s
-          where x' = formatTerm (fst x) ctx
+          where x' = formatTerm (fst x) env
       _ -> cannotParse s
     else if head s == 't'
     then case apply term $ tail s of
@@ -49,24 +50,25 @@ repl ctx = do
         case typeof' t' of
           Just y -> putStrLn . prependTerm t' $ reduce t'
           _ -> cannotType s
-          where t' = formatTerm t ctx 
+          where t' = formatTerm t env 
       [((v,t),"")] -> do       -- let expression
         case typeof' t of
           Just y -> do 
             putStrLn $ "Saved: " ++ show t
-            repl ((v,t):ctx)
+            repl $ M.insert v t' env
+            where t' = formatTerm t env
           _ -> cannotType s
       _ -> cannotParse s  
-    repl ctx
+    repl env
 
--- takes a term and context and substitutes context terms
+-- takes a term and context and substitutes env terms
 -- all free occurrences in the term
 formatTerm :: PCFTerm -> Environment -> PCFTerm
-formatTerm t1 [] = t1
-formatTerm t1 ((v,t2):xs) = if elem v vs 
-  then formatTerm (substitute t1 (Var v, t2)) xs
-  else formatTerm t1 xs
-    where vs = vars t1
+formatTerm t1 env = foldl 
+  (\t (v,t2) -> 
+    if elem v (vars t1) 
+    then substitute t (Var v, t2) 
+    else t) t1 $ M.assocs env
 
 --function prepends ~> arrows or prints existing term if no reds occur
 prependReductions :: PCFTerm -> [PCFTerm] -> [String]
