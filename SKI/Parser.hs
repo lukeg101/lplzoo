5b259bef4f0c052bbdc01291fe8698a4838ce0f4
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-02-03 10:30:45 +0000

    [SKI] Parser module linted, walled, and haddocked

--- SKI/Parser.hs
+++ SKI/Parser.hs
@@ -1,80 +1,118 @@
+{-|
+Module      : Parser
+Description : Monadic Parser Combinators for the untyped lambda calculus in Haskell.
+Copyright   : (c) Luke Geeson, 2018
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Parser" module provides the monadic parser combinators, grammars, and top-level functions needed to parse a human friendly (read whiteboard) version of SKI.
+-}
 module Parser where
 
-import SKI
-import Control.Applicative (Applicative(..))
-import Control.Monad       (liftM, ap, guard)
-import Data.Char
+-- SKI imports.
+import qualified SKI
+
+-- Tool Imports.
+import qualified Control.Applicative as A (Applicative(..)) 
+import qualified Control.Monad       as M (liftM, ap)
+import qualified Data.Char           as C
 
 {-
 Implementation based on ideas in Monadic Parser Combinators paper
 http://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf
 -}
 
--- Parser type takes input string and returns a list of possible parses
+-- | Parser type takes input string and returns a list of possible parses.
 newtype Parser a = Parser (String -> [(a, String)])
 
--- Necessary AMP additions for Parser instance
+
+-- | Necessary AMP additions for Parser instance.
 instance Functor Parser where
-  fmap = liftM
+  fmap = M.liftM
+
+
+-- | Necessary AMP additions for Parser instance.
 instance Applicative Parser where
   pure a = Parser (\cs -> [(a,cs)])
-  (<*>) = ap
+  (<*>)  = M.ap
 
--- Monad instance, generators use the first parser then apply f to the result
+
+-- | Monad instance, generators use the first parser then apply f to the result
 instance Monad Parser where
-  return = pure
+  return  = pure
   p >>= f = Parser (\cs -> concat [parse (f a) cs' | (a,cs') <- parse p cs])
 
--- parser deconstructor
+
+-- | Parser deconstructor.
+parse :: Parser a -> String -> [(a, String)]
 parse (Parser p) = p
 
--- takes a string and splits on the first char or fails
-item :: Parser Char
-item = Parser (\cs -> case cs of
-  "" -> []
-  (c:cs) -> [(c,cs)])
 
--- combines the results of 2 parsers on an input string
+-- | Item takes a string and splits on the first char or fails
+item :: Parser Char
+item = let split cs = case cs of
+                        ""     -> []
+                        (c:cs) -> [(c,cs)]
+       in Parser split
+
+
+-- | Combines the results of 2 parsers on an input string
 -- shortcircuits on the first result returned or fails
 (+++) :: Parser a -> Parser a -> Parser a
-p +++ q = Parser (\cs -> case parse p cs ++ parse q cs of
-  [] -> []
-  (x:xs) -> [x])
+p +++ q = let apply cs = case parse p cs ++ parse q cs of
+                          []    -> []
+                          (x:_) -> [x]
+          in Parser apply 
 
--- failure parser
-zerop = Parser (\cs -> [])
 
--- parses an element and returns if they satisfy a predicate
+-- | Failure parser.
+zerop = Parser (const [])
+
+
+-- | Parses an element and returns if they satisfy a predicate.
 sat :: (Char -> Bool) -> Parser Char
-sat p = do {c <- item; if p c then return c else zerop}
+sat p = do 
+  c <- item
+  if p c 
+    then return c 
+    else zerop
 
--- parses chars only
+
+-- | Parses chars only.
 char :: Char -> Parser Char
 char c = sat (c ==)
 
--- parses a string of chars
+
+-- | Parses a string of chars.
 string :: String -> Parser String
 string = mapM char
 
--- parses 0 or more elements
+
+-- | Parses 0 or more elements.
 many :: Parser a -> Parser [a]
 many p = many1 p +++ return []
 
--- parses 1 or more elements
+
+-- | Parses 1 or more elements.
 many1 :: Parser a -> Parser [a]
 many1 p = do
-  a <- p
+  a  <- p
   as <- many p
   return (a:as)
 
--- parses 0 or more whitespace
+
+-- | Parses 0 or more whitespace.
 space :: Parser String
-space = many (sat isSpace)
+space = many (sat C.isSpace)
 
+-- | Parsers 1 or more whitespace.
 space1 :: Parser String
-space1 = many1 (sat isSpace)
+space1 = many1 (sat C.isSpace)
 
--- trims whitespace between an expression
+
+-- | Trims whitespace between an expression.
 spaces :: Parser a -> Parser a 
 spaces p = do
   space
@@ -82,30 +120,36 @@ spaces p = do
   space
   return x
 
--- parses a single string
+-- | Parses a single string.
 symb :: String -> Parser String
 symb = string
 
--- apply a parser to a string
-apply :: Parser a -> String -> [(a,String)]
-apply p = parse p
 
--- left recursion 
+-- | Apply a parser to a string.
+apply :: Parser a -> String -> [(a,String)]
+apply = parse
+
+
+-- | Left recursion. 
 chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-p `chainl1` op = do {a <- p; rest a}
-  where
-    rest a = (do 
-      f <- op
-      b <- p
-      rest (f a b)) +++ return a
+p `chainl1` op = let rest a = (do f <- op
+                                  b <- p
+                                  rest (f a b)) +++ return a
+
+                 in do a <- p
+                       rest a 
+
     
--- 1 or more chars
+-- | Parser 1 or more chars (a string).
 str :: Parser String
 str = do 
-  s <- many1 $ sat isLower
-  if elem s ["let", "=", "S", "K", "I"] then zerop else return s
+  s <- many1 $ sat C.isLower
+  if s `elem` ["let", "="] 
+    then zerop 
+    else return s
 
--- bracket parses away brackets as you'd expect
+
+-- | Parses away brackets as you'd expect.
 bracket :: Parser a -> Parser a
 bracket p = do
   symb "("
@@ -113,50 +157,62 @@ bracket p = do
   symb ")"
   return x
 
--- vars are nats packaged up
-var = do
-  x <- str
-  return (Var x)
 
+-- | Vars are strings packaged up.
+var :: Parser SKI.SKTerm
+var = SKI.Var <$> str
+
+
+-- | Parser for S terms
+s :: Parser SKI.SKTerm
 s = do 
   char 'S'
-  return S
+  return SKI.S
 
+
+-- | Parser for K terms
+k :: Parser SKI.SKTerm
 k = do 
   char 'K'
-  return K
+  return SKI.K
 
+
+-- | Parser for I terms
+i :: Parser SKI.SKTerm
 i = do 
   char 'I'
-  return I
+  return SKI.I
 
--- app has one or more spaces
+
+-- | App parses application terms, with one or more spaces in between terms.
+app :: Parser SKI.SKTerm
 app = chainl1 expr $ do
   space1
-  return $ App 
+  return SKI.App 
 
--- parser for let expressions
+
+-- | Parser for let expressions
 pLet = do
   space
   symb "let"
   space1
   v <- str
   spaces $ symb "="
-  t <- spaces $ term 
+  t <- term 
   return (v,t)
 
+
+-- | Parser for regular terms.
 pTerm = do
   t <- term 
   return ("", t)
 
--- expression follows CFG form with bracketing convention
-expr = (bracket term) +++ var +++ s +++ k +++ Parser.i
+-- | Expression follows CFG form with bracketing convention.
+expr = bracket term +++ var +++ s +++ k +++ i
 
--- top level of CFG Gramma
+-- | Top-level of CFG Grammar.
 term = app
 
--- identifies key words
-identifier :: [Char] -> Parser Char 
-identifier xs = do
-  x <- sat (\x -> elem x xs)
-  return x
+-- | Identifies key words.
+identifier :: String -> Parser Char 
+identifier xs = sat (`elem` xs)
