7ee72248391fe97d9102d8363b6083e735f85da9
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-02-03 11:00:10 +0000

    [SKI] Repl and Main modules linted, walled, and haddocked

--- SKI/Main.hs
+++ SKI/Main.hs
@@ -1,7 +1,20 @@
+{-|
+Module      : Main
+Description : The Main access point for SKI.
+Copyright   : (c) Luke Geeson, 2018
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Main" module provides the access point for running SKI. This is
+targeted by main when compiled and run.
+-}
 module Main where
 
+-- SKI imports.
 import Repl
 
--- Simple SKI Repl
+-- | Simple SKI Repl
 main :: IO ()
 main = replMain
--- SKI/Repl.hs
+++ SKI/Repl.hs
@@ -1,66 +1,99 @@
+{-|
+Module      : Repl
+Description : The Read-Eval-Print-Loop for SKI.
+Copyright   : (c) Luke Geeson, 2018
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Repl" module provides the Read-eval-print-loop for SKI.
+This is the top level interface between the user and the interpreter.
+-}
 module Repl where
 
-import SKI
+-- SKI imports.
+import qualified SKI
 import Parser
+  (pLet, pTerm, apply, term, (+++))
 
+
+-- Tool Imports.
 import System.IO           (hFlush, stdout)
 import qualified Data.Map.Lazy as M 
 
--- top level repl function
+
+-- | Top-level repl function
 replMain :: IO ()
 replMain = do
   putStrLn "Welcome to the SKI combinator calculus REPL"
   putStrLn "Type some terms or press Enter to leave."
   repl M.empty
 
--- stores variables from let expressions at runtime
-type Environment = M.Map String SKTerm
 
--- REPL loop, takes input reduces and prints result, or exits out
+-- | Stores variables from let expressions at runtime
+type Environment = M.Map String SKI.SKTerm
+
+
+-- | REPL loop, takes input reduces and prints result, or exits out.
 repl :: Environment -> IO ()
-repl env = do
-  putStr ">   "
-  hFlush stdout
-  s <- getLine
-  if Prelude.null s 
-  then putStrLn "Goodbye."
-  else do
-    if head s == '\'' 
-    then case apply term (tail s) of
-      (x:xs) -> do
-        if (null $ snd x)
-        then mapM_ putStrLn . prependReductions (fst x) . reductions $ fst x
-        else cannotParse s
-      _ -> cannotParse s
-    else case apply (pLet +++ pTerm) s of
-      [(("",t),"")] -> do      -- reducing a term
-        putStrLn . prependTerm t' $ reduce t'
-        where t' = formatTerm t env 
-      [((v,t),"")] -> do       -- let expression
-        putStrLn $ "Saved: " ++ show t'
-        repl $ M.insert v t' env
-        where t' = formatTerm t env
-      x -> cannotParse s  
-  repl env
+repl env = do putStr ">   "
+              hFlush stdout
+              s <- getLine
+              if null s 
+                then putStrLn "Goodbye."
+                else do {parseTerm s env; repl env}
 
--- takes a term and context and substitutes env terms
--- all free occurrences in the term
-formatTerm :: SKTerm -> Environment -> SKTerm
+
+-- | Takes a term and context and substitutes env terms
+-- into all free occurrences in the term
+formatTerm :: SKI.SKTerm -> Environment -> SKI.SKTerm
 formatTerm t1 env = foldl 
   (\t (v,t2) -> 
-    if elem v (vars t1) 
-    then substitute t (Var v, t2) 
-    else t) t1 $ M.assocs env
+    if v `elem` SKI.vars t1 
+      then SKI.substitute t (SKI.Var v, t2) 
+      else t) t1 $ M.assocs env
 
---function prepends ~> arrows or prints existing term if no reds occur
-prependReductions :: SKTerm -> [SKTerm] -> [String]
+
+-- | Function prepends ~> arrows or prints existing term if no reds occur
+prependReductions :: SKI.SKTerm -> [SKI.SKTerm] -> [String]
 prependReductions x xs = if null xs then ["=   "++show x] else 
   map (\x -> "~>  " ++ show x) xs
 
---function prepends reduction ops for one multi-step reduction
-prependTerm :: SKTerm -> SKTerm -> String
+
+-- | Helper function to print the reduction steps of a term
+printReductions :: String -> IO ()
+printReductions s 
+  = let prepend x = prependReductions (fst x) . SKI.reductions $ fst x
+    in case apply term (tail s) of
+      (x:_) -> if null $ snd x
+                 then mapM_ putStrLn $ prepend x
+                 else cannotParse s
+      _     -> cannotParse s
+
+-- | Helper function parses the term as either a Let expression or a raw term.
+-- Let expressions update the environment with new variables, using any existing
+-- variables in scope. For a raw term, evaluation is attempted after substituting
+-- in bound terms from the environment. For SKI termination is not guaranteed.
+parseTerm :: String -> Environment -> IO ()
+parseTerm s env
+  = if head s == '\'' 
+      then printReductions s
+      else case apply (pLet +++ pTerm) s of
+             [(("",t),"")] -> putStrLn . prependTerm t' $ SKI.reduce t'
+                              where t' = formatTerm t env 
+             [((v,t),"")]  -> do putStrLn $ "Saved: " ++ show t'
+                                 repl $ M.insert v t' env
+                              where t' = formatTerm t env
+             _             -> cannotParse s
+
+
+-- | Function prepends reduction ops for one multi-step reduction
+prependTerm :: SKI.SKTerm -> SKI.SKTerm -> String
 prependTerm x y = if x == y then "=   " ++ show x else "~>* " ++ show y
 
+
+-- | Error message for parse failure
 cannotParse :: String -> IO ()
 cannotParse s = putStrLn $ (++) "Cannot Parse Term: " s
 
