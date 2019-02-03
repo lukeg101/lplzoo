c720e6ca3621c740a0319156a64d865afa0c38e9
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-02-03 15:51:10 +0000

    [SystemT] Repl and Main linted, walled, and haddocked

--- SystemT/Main.hs
+++ SystemT/Main.hs
@@ -1,7 +1,20 @@
+{-|
+Module      : Main
+Description : The Main access point for STLC.
+Copyright   : (c) Luke Geeson, 2018
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Main" module provides the access point for running SystemT. This is
+targeted by main when compiled and run.
+-}
 module Main where
 
+-- System T imports.
 import Repl
 
--- Simple System T Repl
+-- | Simple System T Repl
 main :: IO ()
 main = replMain
