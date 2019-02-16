1e73ac12668e63c48850ba4208b53ed0e892f3bd
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-02-16 00:20:29 +0000

    [PCF] Main haddocked, walled, and linted

--- PCF/Main.hs
+++ PCF/Main.hs
@@ -1,7 +1,21 @@
+{-|
+Module      : Main
+Description : The Main access point for PCF.
+Copyright   : (c) Luke Geeson, 2019
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Main" module provides the access point for running PCF. This is
+targeted by main when compiled and run.
+-}
 module Main where
 
+
+-- PCF Imports.
 import Repl
 
--- Simple PCF Repl
+-- | Simple PCF Repl
 main :: IO ()
 main = replMain
