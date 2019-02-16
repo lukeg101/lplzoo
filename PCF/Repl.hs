bffb75e50c6d803cdeffbb57430b3aeac9108c88
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-02-16 00:05:48 +0000

    [PCF] Repl haddocked, walled, and linted

--- PCF/Repl.hs
+++ PCF/Repl.hs
@@ -1,86 +1,122 @@
+{-|
+Module      : Repl
+Description : The Read-Eval-Print-Loop for PCF.
+Copyright   : (c) Luke Geeson, 2018
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Repl" module provides the Read-eval-print-loop for PCF.
+This is the top level interface between the user and the interpreter.
+-}
 module Repl where
 
+-- PCF Imports.
 import PCF
 import Parser
 
-import System.IO           (hFlush, stdout)
-import Control.Monad       (guard)
+
+-- Tool Imports.
+import qualified System.IO     as IO (hFlush, stdout)
 import qualified Data.Map.Lazy as M 
 
--- top level repl function
+-- | Top level repl function
 replMain :: IO ()
 replMain = do
   putStrLn "Welcome to the PCF REPL"
   putStrLn "Type some terms or press Enter to leave."
   repl M.empty
 
--- stores variables from let expressions at runtime
+-- | Stores variables from let expressions at runtime
 type Environment = M.Map String PCFTerm
 
--- REPL loop, takes input reduces and prints result, or exits out
-repl :: Environment -> IO ()
-repl env = do
-  putStr ">   "
-  hFlush stdout
-  s <- getLine
-  if Prelude.null s 
-  then putStrLn "Goodbye."
-  else do
-    if head s == '\'' 
-    then case apply term (tail s) of
-      (x:xs) -> do
-        if (null $ snd x)
-        then case typeof' x' of
-          Just y -> mapM_ putStrLn . prependReductions x' $ reductions x'
-          _ -> cannotType $ tail s
-        else cannotParse s
-          where x' = formatTerm (fst x) env
-      _ -> cannotParse s
-    else if head s == 't'
-    then case apply term $ tail s of
-      (x:xs) -> do
-        if (null $ snd x)
-        then case typeof' $ fst x of
-          Just y -> print y
-          _ -> cannotType $ tail s
-        else cannotParse s
-      _ -> cannotParse s
-    else case apply (pLet +++ pTerm) s of
-      [(("",t),"")] -> do      -- reducing a term
-        case typeof' t' of
-          Just y -> putStrLn . prependTerm t' $ reduce t'
-          _ -> cannotType s
-          where t' = formatTerm t env 
-      [((v,t),"")] -> do       -- let expression
-        case typeof' t' of
-          Just y -> do 
-            putStrLn $ "Saved: " ++ show t'
-            repl $ M.insert v t' env
-            where t' = formatTerm t env
-          _ -> cannotType s
-      _ -> cannotParse s  
-    repl env
 
--- takes a term and context and substitutes env terms
--- all free occurrences in the term
+-- | REPL loop, takes input reduces and prints result, or exits out
+repl :: Environment -> IO ()
+repl env = do putStr ">   "
+              IO.hFlush IO.stdout
+              s <- getLine
+              if null s 
+                then putStrLn "Goodbye."
+                else do {parseTerm s env; repl env}
+
+
+-- | Helper function parses the term as either a Let expression or a raw term.
+-- Let expressions update the environment with new variables, using any existing
+-- variables in scope. For a raw term, evaluation is attempted after substituting
+-- in bound terms from the environment.
+parseTerm :: String -> Environment -> IO ()
+parseTerm s env
+  | head s == '\'' = printReductions s env
+  | head s == 't'  = printType s
+  | otherwise = case apply (pLet +++ pTerm) s of
+                  [(("",t),"")] -> -- reducing a term
+                    case typeof' t' of
+                      Just _ -> putStrLn . prependTerm t' $ reduce t'
+                      _      -> cannotType s
+                      where t' = formatTerm t env 
+                  [((v,t),"")]  ->  -- let expression
+                    case typeof' t' of
+                      Just _ -> do putStrLn $ "Saved: " ++ show t'
+                                   repl $ M.insert v t' env
+                      _      -> cannotType s
+                   where t' = formatTerm t env
+                  _             -> cannotParse s
+
+
+-- | Helper function to print the reduction steps of a term
+printReductions :: String -> Environment -> IO ()
+printReductions s env 
+  = let prepend x = prependReductions x $ reductions x
+    in case apply term (tail s) of
+         (x:_) -> if null $ snd x
+                    then case typeof' x' of
+                      Just _ -> mapM_ putStrLn $ prepend x'
+                      _      -> cannotType $ tail s
+                    else cannotParse s
+          where x' = formatTerm (fst x) env
+         _     -> cannotParse s
+
+
+-- | Helper function to print the type of a term
+printType :: String -> IO ()
+printType s = case apply term $ tail s of
+  (x:_) -> if null $ snd x
+             then case typeof' $ fst x of
+                    Just y -> print y
+                    _      -> cannotType $ tail s
+             else cannotParse s
+  _     -> cannotParse s
+
+
+-- | Takes a term and context and substitutes env terms
+-- into all free occurrences in the term
 formatTerm :: PCFTerm -> Environment -> PCFTerm
 formatTerm t1 env = foldl 
   (\t (v,t2) -> 
-    if elem v (vars t1) 
-    then substitute t (Var v, t2) 
-    else t) t1 $ M.assocs env
+    if v `elem` vars t1 
+      then substitute t (Var v, t2) 
+      else t) t1 $ M.assocs env
 
---function prepends ~> arrows or prints existing term if no reds occur
+
+-- | Function prepends ~> arrows or prints existing term if no reds occur
 prependReductions :: PCFTerm -> [PCFTerm] -> [String]
 prependReductions x xs = if null xs then ["=   "++show x] else 
   map (\x -> "~>  " ++ show x) xs
 
---function prepends reduction ops for one multi-step reduction
+
+-- | Function prepends reduction ops for one multi-step reduction
 prependTerm :: PCFTerm -> PCFTerm -> String
 prependTerm x y = if x == y then "=   " ++ show x else "~>* " ++ show y
 
+
+-- | Error message for parse failure
 cannotParse :: String -> IO ()
 cannotParse s = putStrLn $ (++) "Cannot Parse Term: " s
 
+
+-- | Error message for typing failure
 cannotType :: String -> IO ()
-cannotType s = putStrLn $ (++) "Cannot Type Term: " $ s
+cannotType s = putStrLn $ (++) "Cannot Type Term: " s
+
