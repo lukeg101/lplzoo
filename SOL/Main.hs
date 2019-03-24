5dccb381b5a739c3779da0057e192019bbcb07b2
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-03-24 19:51:18 +0000

    [SOL] Haddocked Main

--- SOL/Main.hs
+++ SOL/Main.hs
@@ -1,7 +1,20 @@
+{-|
+Module      : Main
+Description : The Main access point for SOL.
+Copyright   : (c) Luke Geeson, 2019
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Main" module provides the access point for running SOL. This is
+targeted by main when compiled and run.
+-}
 module Main where
 
+-- SOL Imports.
 import Repl
 
--- Simple System F Repl
+-- | Simple SOL Repl
 main :: IO ()
 main = replMain
