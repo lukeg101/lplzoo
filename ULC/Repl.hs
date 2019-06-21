1c1bbbbf88507a451dc4e34604f6512cd1ea4e43
Colm Baston <colm@colmbaston.co.uk>
2019-06-21 18:04:06 +0100

    added Haskeline support to REPL

--- ULC/Repl.hs
+++ ULC/Repl.hs
@@ -3,7 +3,7 @@ Module      : Repl
 Description : The Read-Eval-Print-Loop for ULC.
 Copyright   : (c) Luke Geeson, 2018
 License     : GPL-3
-Maintainer  : mail@lukegeeson.com 
+Maintainer  : mail@lukegeeson.com
 Stability   : stable
 Portability : POSIX
 
@@ -13,52 +13,61 @@ This is the top level interface between the user and the interpreter.
 module Repl where
 
 -- ULC Imports.
-import ULC         
+import ULC
   (Term(Var), substitute, vars, reductions, reduce)
-import Parser 
+import Parser
   (apply, term, pLet, pTerm, (+++))
 
 
 -- Tool Imports.
-import System.IO             (hFlush, stdout)
-import qualified Data.Map.Lazy as M 
+import qualified Data.Map.Lazy as M
 
+-- Haskeline imports
+import Control.Monad.Trans
+import Control.Monad.Trans.State.Strict
+import System.Console.Haskeline
 
 -- | Top-level repl function
 replMain :: IO ()
 replMain = do
   putStrLn "Welcome to the Untyped \x03bb-calculus REPL"
   putStrLn "Type some terms or press Enter to leave."
-  repl M.empty
+  evalStateT (runInputT settings repl) M.empty
 
+settings :: MonadException m => Settings m
+settings = Settings noCompletion Nothing True
+
+interruptible :: MonadException m => a -> InputT m a -> InputT m a
+interruptible d x = handle (\Interrupt -> outputStrLn "interrupted" >> pure d) (withInterrupt x)
 
 -- | Stores variables from let expressions at runtime
 type Environment = M.Map String ULC.Term
-
+type Interpreter = InputT (StateT Environment IO)
 
 -- | REPL loop, takes input reduces and prints result, or exits out.
-repl :: Environment -> IO ()
-repl env = do putStr ">   "
-              hFlush stdout
-              s <- getLine
-              if null s 
-                then putStrLn "Goodbye."
-                else do {parseTerm s env; repl env}
-
+repl :: Interpreter ()
+repl = do ms <- getInputLine ">   "
+          case ms of
+            Nothing -> repl
+            Just "" -> outputStrLn "Goodbye."
+            Just s  -> do env  <- lift get
+                          env' <- interruptible env (liftIO (parseTerm s env))
+                          lift (put env')
+                          repl
 
 -- | Takes a term and context and substitutes env terms
 -- into all free occurrences in the term
 formatTerm :: ULC.Term -> Environment -> ULC.Term
-formatTerm t1 env = foldl 
-  (\t (v,t2) -> 
-    if v `elem` ULC.vars t1 
-      then ULC.substitute t (ULC.Var v, t2) 
+formatTerm t1 env = foldl
+  (\t (v,t2) ->
+    if v `elem` ULC.vars t1
+      then ULC.substitute t (ULC.Var v, t2)
       else t) t1 $ M.assocs env
 
 
 -- | Function prepends ~> arrows or prints existing term if no reds occur
 prependReductions :: ULC.Term -> [ULC.Term] -> [String]
-prependReductions x xs = if null xs then ["=   "++show x] else 
+prependReductions x xs = if null xs then ["=   "++show x] else
   map (\x -> "~>  " ++ show x) xs
 
 
@@ -74,7 +83,7 @@ cannotParse s = putStrLn $ (++) "Cannot Parse Term: " s
 
 -- | Helper function to print the reduction steps of a term
 printReductions :: String -> IO ()
-printReductions s 
+printReductions s
   = let prepend x = prependReductions (fst x) . ULC.reductions $ fst x
     in case apply term (tail s) of
          (x:_) -> if null (snd x)
@@ -87,17 +96,15 @@ printReductions s
 -- Let expressions update the environment with new variables, using any existing
 -- variables in scope. For a raw term, evaluation is attempted after substituting
 -- in bound terms from the environment. For ULC termination is not guaranteed.
-parseTerm :: String -> Environment -> IO ()
-parseTerm s env 
-  = if head s == '\'' 
-      then printReductions s
+parseTerm :: String -> Environment -> IO Environment
+parseTerm s env
+  = if head s == '\''
+      then printReductions s >> pure env
       else case apply (pLet +++ pTerm) s of
-             [(("",t),"")] -> putStrLn . prependTerm t' $ reduce t'
-                              where t' = formatTerm t env 
-             [((v,t),"")]  -> do putStrLn $ "Saved: " ++ show t'
-                                 repl $ M.insert v t' env
-                              where t' = formatTerm t env
-             _             -> cannotParse s
-
-
-             
+             [(("",t),"")] -> do let t' = formatTerm t env
+                                 putStrLn (prependTerm t' $ reduce t')
+                                 pure env
+             [((v,t),"")]  -> do let t' = formatTerm t env
+                                 putStrLn $ "Saved: " ++ show t'
+                                 pure (M.insert v t' env)
+             _             -> cannotParse s >> pure env
