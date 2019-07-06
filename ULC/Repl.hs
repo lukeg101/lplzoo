3eea6ea93bac51bc5652140a988031367efe9b09
Colm Baston <colm@colmbaston.co.uk>
2019-07-06 12:04:46 +0100

    [ULC] overhauled command parsing to use a dedicated Command type

--- ULC/Repl.hs
+++ ULC/Repl.hs
@@ -14,16 +14,13 @@ module Repl where
 
 -- ULC Imports.
 import ULC
-  (Term(Var), substitute, vars, reductions, reduce)
 import Parser
-  (apply, term, pLet, pTerm, (+++))
+  (Command(..), parseReplCommand)
 
 
 -- Tool Imports.
-import           Data.Char
 import           Data.List
-import           Data.Foldable
-import qualified Data.Map.Lazy as M
+import qualified Data.Map as M
 
 import Control.Monad.Trans
 import Control.Monad.Trans.State.Strict
@@ -59,77 +56,61 @@ settings = Settings contextCompletion Nothing True
 interruptible :: MonadException m => a -> InputT m a -> InputT m a
 interruptible d x = handle (\Interrupt -> outputStrLn "interrupted" >> pure d) (withInterrupt x)
 
-
 -- | REPL loop, takes input reduces and prints result, or exits out.
 repl :: Interpreter ()
 repl = do ms <- getInputLine ">   "
           case ms of
             Nothing -> repl
             Just "" -> outputStrLn "Goodbye."
-            Just s  -> do env  <- lift get
-                          env' <- interruptible env (liftIO (parseTerm s env))
-                          lift (put env')
+            Just s  -> do case parseReplCommand s of
+                            Nothing -> outputStrLn "Could not parse!"
+                            Just  c -> do env <- lift get
+                                          interruptible () (handleCommand (formatCommand env c))
                           repl
 
+-- Dispatches a procedure for each type of command.
+handleCommand :: Command -> Interpreter ()
+handleCommand (T     t) = outputStrLn (prependTerm t (reduce t))
+handleCommand (Reds  t) = mapM_ outputStrLn (prependedReductions t)
+handleCommand (Load fp) = handleLoad fp
+handleCommand (Let v t) = do lift get >>= lift . put . M.insert v t
+                             outputStrLn ("Saved term " ++ show t)
 
--- | Takes a term and context and substitutes env terms
--- into all free occurrences in the term
-formatTerm :: ULC.Term -> Environment -> ULC.Term
-formatTerm t1 env = foldl
-  (\t (v,t2) ->
-    if v `elem` ULC.vars t1
-      then ULC.substitute t (ULC.Var v, t2)
-      else t) t1 $ M.assocs env
-
+-- | Substitutes named terms from the environment into
+-- free occurrences of those names within command terms
+formatCommand :: Environment -> Command -> Command
+formatCommand env c = case c of
+                        T     t -> T     (formatTerm t)
+                        Reds  t -> Reds  (formatTerm t)
+                        Let s t -> Let s (formatTerm t)
+                        c       -> c
+  where
+    formatTerm :: Term -> Term
+    formatTerm = flip (M.foldlWithKey (\t1 v t2 -> ULC.substitute t1 (ULC.Var v, t2))) env
 
 -- | Function prepends ~> arrows or prints existing term if no reds occur
-prependReductions :: ULC.Term -> [ULC.Term] -> [String]
-prependReductions x xs = if null xs then ["=   "++show x] else
-  map (\x -> "~>  " ++ show x) xs
-
+prependedReductions :: Term -> [String]
+prependedReductions t = let xs = reductions t in if null xs
+                                                   then ["=   " ++ show t]
+                                                   else map (\x -> "~>  " ++ show x) xs
 
 -- | Function prepends reduction ops for one multi-step reduction
 prependTerm :: ULC.Term -> ULC.Term -> String
 prependTerm x y = if x == y then "=   " ++ show x else "~>* " ++ show y
 
+-- | Helper function to load and run a script of interpreter commands
+handleLoad :: FilePath -> Interpreter ()
+handleLoad fp = do exists <- liftIO (doesFileExist fp)
+                   if exists
+                     then do e <- parseCommands <$> liftIO (readFile fp)
+                             case e of
+                               Left  n  -> outputStrLn ("Could not parse line " ++ show n ++ "!")
+                               Right cs -> mapM_ handleCommand cs
+                     else outputStrLn "File doesn't exist!"
+  where
+    parseCommands :: String -> Either Int [Command]
+    parseCommands = sequence . map (toEither . fmap parseReplCommand) . filter (not . null . snd) . zip [1..] . lines
 
--- | Helper function parses the term as either a Let expression or a raw term.
--- Let expressions update the environment with new variables, using any existing
--- variables in scope. For a raw term, evaluation is attempted after substituting
--- in bound terms from the environment. For ULC termination is not guaranteed.
-parseTerm :: String -> Environment -> IO Environment
-parseTerm ('\'':s) env = printReductions s >> pure env
-parseTerm ( 'f':s) env = handleFile (dropWhile isSpace s) env
-parseTerm       s  env = handleTerm s env
-
--- | Helper function to print the reduction steps of a term
-printReductions :: String -> IO ()
-printReductions s
-  = let prepend x = prependReductions (fst x) . ULC.reductions $ fst x
-    in case apply term (tail s) of
-         (x:_) -> if null (snd x)
-                    then mapM_ putStrLn $ prepend x
-                    else cannotParse s
-         _     -> cannotParse s
-
--- | Helper function to run a script of interpreter commands
-handleFile :: FilePath -> Environment -> IO Environment
-handleFile path env = do exists <- doesFileExist path
-                         if exists
-                           then filter (not . null) . lines <$> readFile path >>= foldlM (flip parseTerm) env
-                           else putStrLn "File doesn't exist!" >> pure env
-
--- | Helper function to parse a term, and either reduce it or to add it to the environment
-handleTerm :: String -> Environment -> IO Environment
-handleTerm s env = case apply (pLet +++ pTerm) s of
-                     [(("",t),"")] -> do let t' = formatTerm t env
-                                         putStrLn (prependTerm t' $ reduce t')
-                                         pure env
-                     [((v,t),"")]  -> do let t' = formatTerm t env
-                                         putStrLn $ "Saved: " ++ show t'
-                                         pure (M.insert v t' env)
-                     _             -> cannotParse s >> pure env
-
--- | Error message for parse failure
-cannotParse :: String -> IO ()
-cannotParse s = putStrLn $ (++) "Cannot Parse Term: " s
+    toEither :: (a, Maybe b) -> Either a b
+    toEither (a, Nothing) = Left  a
+    toEither (_, Just  b) = Right b
