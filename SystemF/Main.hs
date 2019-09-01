e32de98ff3d13166cfdee594d2f723100101de16
lukeg101 <6547672+lukeg101@users.noreply.github.com>
2019-09-01 13:33:29 +0100

    [SystemF] Walled, Haddocked, and Linted Main

--- SystemF/Main.hs
+++ SystemF/Main.hs
@@ -1,7 +1,20 @@
+{-|
+Module      : Main
+Description : The Main access point for SystemF.
+Copyright   : (c) Luke Geeson, 2019
+License     : GPL-3
+Maintainer  : mail@lukegeeson.com 
+Stability   : stable
+Portability : POSIX
+
+The "Main" module provides the access point for running SystemF. This is
+targeted by main when compiled and run.
+-}
 module Main where
 
+-- System F imports.
 import Repl
 
--- Simple System F Repl
+-- | Simple System F Repl
 main :: IO ()
 main = replMain
